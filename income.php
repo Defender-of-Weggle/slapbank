edd58dcfa8a52ea67936f180fe56e2d3ee806af3
Defender-of-Weggle <Divil92@gmx.de>
2023-10-26 19:28:41 +0200

    Daily income added, will create logfile with each usage

---
+++ income.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <title>Income Blyad</title>
+</head>
+<body>
+<?php
+include "db_connect.inc.php";
+date_default_timezone_set('Europe/Berlin');
+$timeStamp = date('d M Y H:i:s');
+global $con;
+$sql = "SELECT contingent, userName, userID FROM user";
+$res = $con->query($sql);
+while ($dsatz = $res->fetch_assoc())
+{
+    $logParagraph = $timeStamp . " ID: " . $dsatz["userID"] . " Username: " . $dsatz["userName"] . " Contingent old to new: " . $dsatz["contingent"] . " => ";
+
+    $userID = $dsatz["userID"];
+    $update = match (true)
+    {
+        $dsatz["contingent"]>30 => $update = $dsatz["contingent"] + 3,
+
+        $dsatz["contingent"]<=30 => $update = $dsatz["contingent"] + 10,
+
+        default => $update = $dsatz["contingent"] + 5
+
+    };
+    $logParagraph .= $update . "\n";
+    echo $logParagraph . "<br>";
+    $logFile = fopen("incomelog.txt", "a") or die("Unable to open File, blyad..");
+    fwrite($logFile, $logParagraph);
+    fclose($logFile);
+
+    $con->query("UPDATE user SET contingent = '$update' WHERE userID = '$userID'");
+}
+
+
+
+
+
+?>
+</body>
+</html>
